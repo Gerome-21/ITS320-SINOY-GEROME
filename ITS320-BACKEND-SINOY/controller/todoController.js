f684031f0374b4bb8d29eb30783253b522774a03
gerome <geromesinoy14@gmail.com>
2025-04-12 22:53:49 +0800

    midterm_2.1

--- ITS320-BACKEND-SINOY/controller/todoController.js
+++ ITS320-BACKEND-SINOY/controller/todoController.js
@@ -4,7 +4,7 @@ async function createTodo(req, res) {
     const { task } = req.body;
 
     if (!task) {
-        return res.status(400).send('Please provide a task.');
+        return res.status(400).send('Please provide a task. Thankyou.');
     }
 
     try {
